c32a40454c662c17611862937e80bacd098bef50
LNNetworks <30953574+LNNetworks@users.noreply.github.com>
2017-08-17 14:58:13 +0200

    Maintenance

--- javascript.js
+++ javascript.js
@@ -1,9 +1,29 @@
+/*
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(800)
+            .delay(300)
+            .fadeOut(250, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
+*/
+
 $('#1, #2, #3, #4').hide();
 
 $('#1').fadeIn(850, function () {
     $('#2').fadeIn(850, function () {
         $('#3').fadeIn(850, function () {
             $('#4').fadeIn(850, function () {
+                $('#4').addClass('glow');
             });
         });
     });
